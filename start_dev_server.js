f7534034c5fbb456cc011b125288c836d96677ba
Leon <54020998+Ruebenritter@users.noreply.github.com>
2022-03-03 11:05:18 +0100

    Bootstrap Page Layout v0.2

--- start_dev_server.js
+++ start_dev_server.js
@@ -6,7 +6,7 @@ import open from "open";
 
 function init() {
     let app = express();
-    app.use("/", express.static("app"));
+    app.use("/", express.static("app/"));
     app.listen(process.env.DEV_PORT, function() {
         console.log("Server started. Opening application in browser ... [Press CTRL + C to stop server]");
         open(`http://localhost:${process.env.DEV_PORT}`);
