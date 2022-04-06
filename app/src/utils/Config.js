047681170f631c69442e50f84d9544e60a6df5a7
goatlikeKarthos <ringdavid99@gmail.com>
2022-04-06 17:06:39 +0200

    Bilder Quadratisch & Kaputte aussortiert. Anzahl Bilder in Hand erhöht

--- app/src/utils/Config.js
+++ app/src/utils/Config.js
@@ -2,7 +2,7 @@ const Config = {
     MAX_ROUNDS: 3,
     MAX_PLAYERS: 8,
     MAX_MEMES: 3,
-    HAND_SIZE: 10,
+    HAND_SIZE: 27,
     MAX_KEYWORDS: 6,
     PLACEHOLDER_MEME: "https://is1-ssl.mzstatic.com/image/thumb/Purple114/v4/a5/3a/b7/a53ab703-a5dc-e293-d8cf-b0b5708889bd/source/256x256bb.jpg",
 
