3918bef5e9dcfd1d949207e60fe3c7ea3d0d91de
goatlikeKarthos <ringdavid99@gmail.com>
2022-04-07 18:42:07 +0200

    Fixed audio levels

--- app/src/Model/GameManager.js
+++ app/src/Model/GameManager.js
@@ -223,17 +223,45 @@ class GameManager extends Observable {
   }
 
   playRatingSound(good){
-    console.log("play rating sound");
+    
     if (good){
       let rand = Math.floor(Math.random() * Config.GOOD_AUDIO_NUM);
+
       let audio = new Audio("/resources/rating_audio/good"+rand+".mp3");
-      audio.volume = 0.1;
+      console.log("play rating sound: good:"+rand);
+      switch(rand){
+        case 2: audio.volume = 0.07; break;
+        case 4: audio.volume = 0.07; break;
+        case 6: audio.volume = 0.07; break;
+        case 7: audio.volume = 0.35; break;
+        case 8: audio.volume = 0.07; break;
+        case 9: audio.volume = 0.17; break;
+        case 10: audio.volume = 0.07; break;
+        case 11: audio.volume = 0.06; break;
+        case 12: audio.volume = 0.17; break;
+        case 13: audio.volume = 0.2; break;
+        case 15: audio.volume = 0.06; break;
+        case 17: audio.volume = 0.06; break;
+        case 18: audio.volume = 0.17; break;
+        case 19: audio.volume = 0.05; break;
+        default: audio.volume = 0.1; break;
+      }
       audio.play();
     }
     else{
       let rand = Math.floor(Math.random() * Config.BAD_AUDIO_NUM);
+      console.log("play rating sound: bad:"+rand);
+
       let audio = new Audio("/resources/rating_audio/bad"+rand+".mp3");
-      audio.volume = 0.1;
+      switch(rand){
+        case 0: audio.volume = 0.07; break;
+        case 2: audio.volume = 0.3; break;
+        case 15: audio.volume = 0.4; break;
+        case 18: audio.volume = 0.2; break;
+        case 20: audio.volume = 0.07; break;
+
+        default: audio.volume = 0.1; break;
+      }
       audio.play();
     }
   }
