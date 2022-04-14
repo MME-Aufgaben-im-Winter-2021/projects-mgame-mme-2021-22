8c72cea13bce22d3703edf62b06838d72d535a19
Alexander Bazo <alexanderbazo@googlemail.com>
2022-04-14 14:33:11 +0200

    Add comments for StoryService

--- app/src/Services/stories/Prompt.js
+++ app/src/Services/stories/Prompt.js
@@ -1,6 +1,10 @@
 const BLANK_PLACEHOLDER = "ˍˍˍˍˍˍˍˍˍˍˍˍˍ",
   BLANK_REGEX = new RegExp(BLANK_PLACEHOLDER, "g");
 
+/**
+ * Repräsentiert einen Story-Prompt, der aus einem String mit beliebig vielen (auch 
+ * keinen) Platzhaltern erstellt wird.
+ */
 class Prompt {
 
   constructor(text) {
